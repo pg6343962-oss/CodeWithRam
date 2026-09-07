b02363d0a6e758b1b65168866e307e01394d2c40
unknown <pg6343962@gmail.com>
2026-09-07 22:48:04 +0545

    Fix debug

--- codewithram/settings.py
+++ codewithram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("=$$w+)xehvcy(=g4j@lk2r&veymopu9opubc=tt7q6bgfronu=")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
